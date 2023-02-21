d0b130c125cea5466329521f365df6dc4c9f47e3
Igor Stepanenko <igorstepanenko696@gmail.com>
2023-02-21 19:48:56 +0000

    missing files

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+        filename:"bundle.css"})
+    ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    compress: true,
+    port: 4000,
+  },
+};
